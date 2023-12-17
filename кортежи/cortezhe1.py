ab997fdb5834c61959d9ce8a6a61bb6b9b405eac
RylyakovaDasshha <146470647+RylyakovaDasshha@users.noreply.github.com>
2023-12-17 20:11:34 +0700

    Update cortezhe1.py

--- кортежи/cortezhe1.py
+++ кортежи/cortezhe1.py
@@ -1,9 +1,12 @@
 def tpl_sort(tpl):
     for i in tpl:
+        #функция проверяет является ли число целым
         if not isinstance(i, int):
+            #если не является целым, то переходит в исходный список
            return tpl
+            #остортированный кортеж
     resulf = (sorted(tpl))
     return resulf
     
 tpl = (1, 8, 9, 7)
-print(tpl_sort(tpl))
+print(tpl_sort(1,8.2.5,7))
